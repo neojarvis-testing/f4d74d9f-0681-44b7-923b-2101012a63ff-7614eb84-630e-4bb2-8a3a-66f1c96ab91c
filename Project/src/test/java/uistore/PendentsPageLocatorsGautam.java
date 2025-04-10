ebde2fb397f70b648b67fc145ea40611edc99d06
gautam95758 <gautamrawat3411@gmail.com>
2025-04-10 18:20:40 +0000

    Initial Commit

---
+++ Project/src/test/java/uistore/PendentsPageLocatorsGautam.java
@@ -0,0 +1,16 @@
+package uistore;
+
+import org.openqa.selenium.By;
+
+public class PendentsPageLocatorsGautam {
+    public static By Gender= By.cssSelector("div[class='head clearfix'][id='myDIV01']");
+    public static By kids=By.cssSelector("a[title='Pendant & Pendant Set - Kids']");
+    public static By moreFilter=By.cssSelector("button[id='myBtn']");
+    public static By type=By.cssSelector("div[title='Pendant & Pendant Set - Type']");
+    public static By pendentInsideType=By.cssSelector("a[title='Pendant & Pendant Set - Pendant']");
+    public static By firstProductOnPendent=By.xpath("(//a[@class='tooltip_18'])[1]");
+    public static By AddtoCart=By.cssSelector("div[id='btnBuyNowC'][class='btnaddtocart']");
+    
+    
+
+}
---
+++ Project/src/test/java/utils/Screenshot.java
@@ -0,0 +1,138 @@
+package utils;
+ 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.io.FileHandler;
+
+import com.aventstack.extentreports.ExtentTest;
+ 
+/**
+* Creator: Akash Deep
+* Description: This class provides methods for capturing screenshots of web pages and web elements,
+* as well as utility methods for managing screenshot directories.
+*/
+public class Screenshot {
+ 
+    public static TakesScreenshot ts;
+ 
+    /**
+     * Description: Captures a screenshot of the entire web page and saves it with a timestamped filename.
+     *
+     * @param filename - The base name for the screenshot file
+     * @return The file path of the saved screenshot
+     */
+    public static String takePageScreenShot(String filename) {
+        String timestamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+        String name = "/" + filename + timestamp + ".png";
+        try {
+            TakesScreenshot ts = (TakesScreenshot) Base.driver;
+            File file = ts.getScreenshotAs(OutputType.FILE);
+            File target = new File(System.getProperty("user.dir") + "/screenshots");
+            deleteAllFilesInsideDirectory(target.toString());
+            target.delete();
+            if (!target.exists()) {
+                target.mkdirs();
+            }
+            FileHandler.copy(file, new File(target.toString() + name));
+            return target.toString() + name;
+        } catch (Exception e) {
+            LoggerHandler.error(e.getMessage());
+        }
+        return null;
+    }
+ 
+    /**
+     * Description: Captures a screenshot of a specific web element and saves it with a timestamped filename.
+     *
+     * @param elementLocator - The locator of the web element to capture
+     * @param fileName - The base name for the screenshot file
+     * @return The file path of the saved screenshot
+     */
+    public static String takeElementScreenShot(By elementLocator, String fileName) {
+        String timestamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+        String name = "/" + fileName + timestamp + ".png";
+        try {
+            WebElement elementParam = Base.driver.findElement(elementLocator);
+            File file = elementParam.getScreenshotAs(OutputType.FILE);
+            File target = new File(System.getProperty("user.dir") + "/screenshots");
+            if (!target.exists()) {
+                target.mkdirs();
+            }
+            FileHandler.copy(file, new File(target.toString() + name));
+            return "." + name;
+        } catch (Exception e) {
+            LoggerHandler.error(e.getMessage());
+        }
+        return null;
+    }
+ 
+    /**
+     * Description: Deletes all files inside the specified directory.
+     *
+     * @param dirPath - The path of the directory to clear
+     */
+    public static void deleteAllFilesInsideDirectory(String dirPath) {
+        File directory = new File(dirPath);
+        if (directory.exists()) {
+            File[] files = directory.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    if (file.isFile()) {
+                        file.delete();
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Captures a screenshot of the highlighted element.
+     *
+     * @param driver   - The WebDriver instance
+     * @param locator  - The locator of the element to highlight and capture
+     * @param filename - The base name for the screenshot file
+     * @return The file path of the saved screenshot
+     */
+ 
+    public static void captureHighlightedElementScreenshot( By locator, String filename, ExtentTest test) {
+ 
+        try {
+            WebDriverHelper helper=new WebDriverHelper(Base.driver, test);
+            WebElement element = Base.driver.findElement(locator);
+            JavascriptExecutor js = (JavascriptExecutor) Base.driver;
+            js.executeScript("arguments[0].setAttribute('style', 'border:solid 5px red');", element);
+ 
+            helper.waitUntilElementIsVisible(locator, 10);
+ 
+           
+ 
+            TakesScreenshot ts = (TakesScreenshot) Base.driver;
+            File file = ts.getScreenshotAs(OutputType.FILE);
+ 
+            File screenshotsDir = new File(System.getProperty("user.dir") + "/screenshorts");
+ 
+            if (!screenshotsDir.exists()) {
+                screenshotsDir.mkdirs();
+            }
+ 
+            String timestamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+ 
+            String name = filename + timestamp + ".png";
+            File target = new File(screenshotsDir, name);
+            FileHandler.copy(file, target);
+ 
+        } catch (IOException e) {
+            e.printStackTrace();
+ 
+        }
+ 
+    }
+
+}
